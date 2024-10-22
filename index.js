91ba33c33aa18a8f7880bc00d0190a9752fef7eb
Resalat islam <resalatislam5@gmail.com>
2024-10-23 01:59:06 +0600

    created search method

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSSWORD}@cl
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
-    strict: true,
+    strict: false,
     deprecationErrors: true,
   },
 });
@@ -29,7 +29,9 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     const tasksCollection = client.db("task").collection("tasks");
-
+    const textIndex = await tasksCollection.createIndex({
+      "$**": "text",
+    });
     // create a task
     app.post("/api/tasks", async (req, res) => {
       const { name, due_date, priority, tags } = req.body;
@@ -44,6 +46,7 @@ async function run() {
       const tasks = await cursor.toArray();
       res.status(200).json(tasks);
     });
+
     // task update method
     app.put("/api/tasks/:id", async (req, res) => {
       const { id } = req.params;
@@ -60,6 +63,7 @@ async function run() {
       );
       res.status(200).json(task);
     });
+
     // delete a task
     app.delete("/api/tasks/:id", async (req, res) => {
       const { id } = req.params;
@@ -68,6 +72,32 @@ async function run() {
       const deletedTask = await tasksCollection.deleteOne(filter);
 
       res.status(200).json(deletedTask);
+    });
+
+    // search a task
+    app.get("/api/tasks/search", async (req, res) => {
+      const { text } = req.query;
+
+      if (!text) {
+        return res.status(400).json({ message: "Search query is required" });
+      }
+
+      try {
+        const cursor = tasksCollection.find({
+          $text: {
+            $search: text,
+          },
+        });
+
+        const searchResults = await cursor.toArray();
+
+        // Send the results back to the client
+        res.status(200).json(searchResults);
+      } catch (error) {
+        // Handle any errors that occur during the query
+        console.error("Error performing search:", error);
+        res.status(500).json({ message: "Internal server error" });
+      }
     });
 
     console.log("connected db");
